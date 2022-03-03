1740d28d799ace715e5ae9749ba98ec2318aac33
Arthurit0 <arthur.jose27@gmail.com>
2022-03-03 06:50:41 -0300

    Praticando useState()

--- Praticando React/praticando/src/App.js
+++ Praticando React/praticando/src/App.js
@@ -1,66 +1,7 @@
-import React, { Component } from "react";
-import "./style.css";
+import React from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      numero: 0,
-      botao: "Start",
-    };
-
-    this.timer = null;
-    this.startTimer = this.startTimer.bind(this);
-    this.reset = this.reset.bind(this);
-  }
-
-  startTimer() {
-    let state = this.state;
-
-    if (this.timer != null) {
-      clearInterval(this.timer);
-      this.timer = null;
-      state.botao = "Start";
-    } else {
-      this.timer = setInterval(() => {
-        state.numero += 0.01;
-        this.setState(state);
-      }, 10);
-
-      state.botao = "Pause";
-    }
-
-    this.setState(state);
-  }
-
-  reset() {
-    if (this.timer != null) {
-      clearInterval(this.timer);
-      this.timer = null;
-    }
-
-    let state = this.state;
-    state.numero = 0;
-    state.botao = "Start";
-    this.setState(state);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <img src={require("./assets/cronometro.png")} className="img" />
-        <a className="timer">{this.state.numero.toFixed(2)}</a>
-        <div className="button-area">
-          <a className="button" onClick={this.startTimer}>
-            {this.state.botao}
-          </a>
-          <a className="button" onClick={this.reset}>
-            Reset
-          </a>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return <div>App</div>;
 }
 
 export default App;
