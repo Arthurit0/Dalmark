52e15fa75915453fce717d889d47d729c9c9fd54
Your Name <you@example.com>
2022-03-14 16:06:00 -0300

    Continuando projeto Filmera/Cineamo

--- Praticando React/praticando/src/components/Header/index.js
+++ Praticando React/praticando/src/components/Header/index.js
@@ -1,14 +1,22 @@
 import "./header.css";
 import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
 export default function Header() {
   return (
     <header>
-      <Link className="logo" to="/">
-        Filmaria
+      <Link className="titulo" to="/">
+        <Icon
+          className="icone"
+          icon="ri:movie-2-line"
+          color="white"
+          width="35"
+        />
+        <span>Cineamo</span>
       </Link>
-      <Link className="favoritos" to="/favoritos">
-        Salvos
+      <Link className="fav" to="/favoritos">
+        <Icon className="icone-fav" icon="bi:bookmark-fill" />
+        <span>Salvos</span>
       </Link>
     </header>
   );
--- Praticando React/praticando/src/pages/Filme/index.js
+++ Praticando React/praticando/src/pages/Filme/index.js
@@ -1,5 +1,6 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Icon } from "@iconify/react";
 
 import api from "../../services/api";
 
@@ -9,16 +10,47 @@ export default function Filme() {
   const { id } = useParams();
   const [filme, setFilme] = useState({});
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function loadFilme() {
       const resp = await api.get(`r-api/?api=filmes/${id}`);
+      if (resp.data.length === 0) {
+        //Se ID não existe, redireciona para home
+        navigate("/", { replace: true });
+        return;
+      }
       setFilme(resp.data);
       setLoading(false);
     }
 
     loadFilme();
-  }, [id]);
+
+    // "componentDidUnmount(), aqui é executado quanto o componente é desmontado"
+    return () => {};
+  }, [navigate, id]);
+
+  function salvaFilme() {
+    const myList = localStorage.getItem("filmes");
+
+    let filmesSalvos = JSON.parse(myList) || []; // Caso não haja items no myList, filmesSalvos será um array vazio.
+
+    // Procuramos se o filme selecionado já está salvo no localStorage
+    const hasFilme = filmesSalvos.some(
+      (filmeSalvo) => filmeSalvo.id === filme.id
+    );
+
+    // Se houver um filme salvo com este ID, alertar e ignorar
+    if (hasFilme) {
+      alert("Você já possui esse filme salvo!");
+      return;
+    }
+
+    // Caso o filme não esteja na storage, salvar
+    filmesSalvos.push(filme);
+    localStorage.setItem("filmes", JSON.stringify(filmesSalvos));
+    alert("Filme salvo com sucesso!");
+  }
 
   if (loading) {
     return (
@@ -28,8 +60,31 @@ export default function Filme() {
     );
   }
   return (
-    <div>
-      <h1>Página do Filme com id: {id}</h1>
+    <div className="filme-info">
+      <h1>{filme.nome}</h1>
+      <img src={filme.foto} alt={filme.nome} />
+      <h3>Sinopse:</h3>
+      {filme.sinopse}
+      <div className="botoes">
+        <button onClick={salvaFilme}>
+          <div className="icon-button">
+            <Icon className="icone" icon="bi:bookmark-fill" />
+            Salvar
+          </div>
+        </button>
+        <button>
+          <div className="icon-button">
+            <Icon className="icone" icon="bxs:movie-play" />
+            <a
+              target="_blank" //Para criar uma nova aba
+              rel="noreferrer" //Medida de segurança contra injeção de HTML em browsers antigos
+              href={`https://www.youtube.com/results?search_query=${filme.nome} Trailer`}
+            >
+              Trailer
+            </a>
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
