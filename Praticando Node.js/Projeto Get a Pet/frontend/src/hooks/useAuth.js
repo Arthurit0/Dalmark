ec7213874835861e140bdd28bf087fca7dbcb0e2
Your Name <you@example.com>
2022-03-29 14:09:55 -0300

    Desenvolvendo Front-End: Conectando form com MongoDB

--- Praticando Node.js/Projeto Get a Pet/frontend/src/hooks/useAuth.js
+++ Praticando Node.js/Projeto Get a Pet/frontend/src/hooks/useAuth.js
@@ -2,19 +2,36 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import useFlashMessage from "./useFlashMessage";
 import api from "../utils/api";
 
 export default function useAuth() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const { setFlashMessage } = useFlashMessage();
+  const navigate = useNavigate();
+
   async function register(user) {
+    let msgText = "Cadastro realizado com sucesso!";
+    let msgType = "success";
+
     try {
-      const data = await api.post("users/register", user).then((resp) => {
-        return resp.data;
+      const data = await api.post("/users/register", user).then((response) => {
+        return response.data;
       });
 
-      console.log(data);
+      await authUser(data);
     } catch (error) {
-      console.log(error);
+      msgText = error.response.data.message;
+      msgType = "error";
     }
+
+    setFlashMessage(msgText, msgType);
+  }
+
+  async function authUser(data) {
+    setAuthenticated(true);
+    localStorage.setItem("token", JSON.stringify(data.token));
+    navigate("/");
   }
 
   return { register };
